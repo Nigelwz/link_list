c2b519b4e6d441e221f89c6e579b82ea3ba48bdd
Eric Wang <asd617140123@gmail.com>
2019-04-26 11:14:18 +0800

    push node from back

---
+++ linklist_push_back/linklist.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+typedef struct _NODE{
+	int data;
+	struct _NODE* next;
+}NODE;
+
+
+NODE* head = NULL;
+
+
+NODE* current;
+
+void insert(int x)
+{
+	NODE* tmp = malloc(sizeof(NODE));
+	//tmp -> data = x;
+	//tmp -> next = head;
+	//head = tmp;
+	tmp -> data = x;
+	tmp -> next = NULL;
+	//head = tmp;
+	/*if(head)
+		tmp -> next = head;
+	head = tmp;*/
+	if(head == NULL)
+		head = tmp;
+	else
+		current ->next = tmp;
+	current = tmp;
+}
+
+void print(void)
+{
+	while(head)
+	{
+		printf(" %d ",head->data);
+		head = head->next;
+	}
+}
+
+void main(void)
+{
+	insert(1);
+	insert(2);
+	insert(3);
+	insert(4);
+	insert(5);
+	print();
+	printf("hello");
+}
